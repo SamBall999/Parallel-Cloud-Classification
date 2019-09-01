75868ccfda3839a5202dc9f014a0a7d18ff26bd1
Samantha Ball <samanthaball@Samanthas-MacBook-Pro-2.local>
2019-09-01 22:56:48 +0200

    added locate method to convert linear position to 3D grid point

--- src/SumArray.java
+++ src/SumArray.java
@@ -3,34 +3,60 @@
 import java.util.concurrent.RecursiveTask;
 import java.util.Vector;
 
+/**
+ * Threaded class to sum a 3D array of (x, y) pairs
+ *
+ *@author Samantha Ball
+ *@version 1.0
+ *@since 1
+ */
 public class SumArray extends RecursiveTask<Vector<Double>>  {
-	  int lo; // arguments
-	  int hi;
+	  int lo; //lower bound
+	  int hi; //upper bound
 		Vector<Float>[][][] advection;
-		int dimx, dimy, dimt;
-		//int xyFlag;
+		int dimx, dimy, dimt;//dimensions of array
 		double xsum = 0;
 		double ysum = 0;
-	  static final int SEQUENTIAL_CUTOFF=2000; //500 //3
+	  static final int SEQUENTIAL_CUTOFF=2000; //vary from 0-50 000
 
 	  int ans = 0; // result
 
-	  SumArray(Vector<Float>[][][] advec, int l, int h) {
+		/**
+	 *Creates a new SumArray instance with the specified parameters
+	 *
+	 *@param advec 3D array of advection values to be added
+	 *@param l Lower bound of elements to be added
+	 *@param h Upper bound of elements to be added
+	 */
+	  public SumArray(Vector<Float>[][][] advec, int l, int h) {
 	    lo=l; hi=h; advection = advec;
 			dimt = advection.length;
 			dimx = advection[0].length;
 			dimy = advection[0][0].length;
 	  }
 
-		// convert linear position into 3D location in simulation grid
-		void locate(int pos, int [] ind)
+
+		/**
+	 * Converts linear position into 3D location in simulation grid
+	 *
+	 *@param pos Linear position to be converted to a grid point
+	 *@param ind Integer array to hold the corresponding grid indices
+	 */
+		public void locate(int pos, int [] ind)
 		{
 			ind[0] = (int) pos / (dimx*dimy); // t
 			ind[1] = (pos % (dimx*dimy)) / dimy; // x
 			ind[2] = pos % (dimy); // y
 		}
 
-
+		/**
+	  * Converts linear position into 3D location in simulation grid
+	  *
+	  *<p>
+	  *Stores datetime, power and voltage values read from each line of the CSV file into a binary search tree node. Keeps track of how many nodes have been stored.
+	  *</p>
+		*@return Vector object containing two float values representing the sum of the x values and the sum of the y values respectively
+		*/
 	  protected Vector<Double> compute(){// return answer - instead of run
 		  if((hi-lo) < SEQUENTIAL_CUTOFF) {
 				//each position represents a specific grid point
